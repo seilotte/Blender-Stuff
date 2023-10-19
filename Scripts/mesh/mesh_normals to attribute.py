698e83c1c4828b61905f196cec004849a75e2a82
seilotte <133937845+seilotte@users.noreply.github.com>
2023-10-19 14:26:05 +0900

    Normals to attribute since -1 to 1 range.

--- Scripts/mesh/mesh_normals to attribute.py
+++ Scripts/mesh/mesh_normals to attribute.py
@@ -4,12 +4,12 @@ from mathutils import Vector
 obj = bpy.context.active_object
 
 obj.data.calc_normals_split()
-new_vcol = obj.data.vertex_colors.new(name='Normals')
+new_attribute = obj.data.attributes.new(name='Normals', domain='CORNER', type='FLOAT_VECTOR')
 
 for face in obj.data.polygons:
     for loop in face.loop_indices:
         normal_vector = Vector((obj.data.loops[loop].normal.x,
                                 obj.data.loops[loop].normal.y,
                                 obj.data.loops[loop].normal.z,
-                                0.0))
-        new_vcol.data[loop].color = normal_vector
+                                ))
+        new_attribute.data[loop].vector = normal_vector
