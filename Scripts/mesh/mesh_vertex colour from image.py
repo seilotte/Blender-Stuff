457342275252b51bd8c99811fe31c2bcdc5ba927
seilotte <133937845+seilotte@users.noreply.github.com>
2024-04-17 05:09:04 +0900

    Vcol from image; Minor fix on "a".

--- Scripts/mesh/mesh_vertex colour from image.py
+++ Scripts/mesh/mesh_vertex colour from image.py
@@ -17,38 +17,41 @@ for obj in context.selected_objects:
     if obj.type != 'MESH': continue
 
 #    obj = context.active_object
+    mesh = obj.data
+
     mat = obj.active_material
     active_node = mat.node_tree.nodes.active # Both return None.
 
-    if mat and active_node:
-        if active_node.type == 'TEX_IMAGE':
+    if not mat \
+    and not active_node \
+    and not active_node.type == 'TEX_IMAGE' \
+    and not active_node.image: continue
 
-            image = active_node.image
-        #    image_name = image.name.split('.')[0] # Remove extension.
-            image_name = "Viewport"
-            image_width, image_height = image.size
-            a = numpy.asarray(image.pixels)
-            a = a.reshape((image_width, image_height, 4))
+    image = active_node.image
+#    image_name = image.name.split('.')[0] # Remove extension.
+    image_name = "Viewport"
+    image_width, image_height = image.size
+    a = numpy.asarray(image.pixels)
+    a = a.reshape((image_height, image_width, 4))
 
-            # Create vertex colour.
-            vcol_layer = obj.data.vertex_colors.get(image_name)
-            if not vcol_layer:
-                vcol_layer = obj.data.vertex_colors.new(name=image_name)
-            obj.data.vertex_colors.active = vcol_layer
+    # Create vertex colour.
+    vcol_layer = mesh.vertex_colors.get(image_name) \
+                or mesh.vertex_colors.new(name=image_name)
+    mesh.vertex_colors.active = vcol_layer
 
-            bm = bmesh.new()
-            bm.from_mesh(obj.data) # object mode
+    bm = bmesh.new()
+    bm.from_mesh(mesh) # object mode
 
-            vcol_layer = bm.loops.layers.color[image_name]
-        #    uv_layer = bm.loops.layers.uv.active
-            uv_layer = bm.loops.layers.uv[0]
+    vcol_layer = bm.loops.layers.color[image_name]
+#    uv_layer = bm.loops.layers.uv.active
+    uv_layer = bm.loops.layers.uv[0]
 
-            for face in bm.faces:
-                for loop in face.loops:
-                    uv_x, uv_y = loop[uv_layer].uv.to_tuple()
-                    xx = int(round( uv_x * (image_width - 1) )) % image_width # xx = pixel_x
-                    yy = int(round( uv_y * (image_height - 1) )) % image_height # yy = pixel_y
-                    loop[vcol_layer] = a[yy][xx]
+    for face in bm.faces:
+        for loop in face.loops:
+            uv_x, uv_y = loop[uv_layer].uv.to_tuple()
+            xx = int(round( uv_x * (image_width - 1) )) % image_width # xx = pixel_x
+            yy = int(round( uv_y * (image_height - 1) )) % image_height # yy = pixel_y
+            loop[vcol_layer] = a[yy][xx]
 
-            bm.to_mesh(obj.data)
-            bm.free()
+    bm.to_mesh(mesh)
+    bm.free()
